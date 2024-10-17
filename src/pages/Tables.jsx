2e4d0830bf4ec2add3f2114822be8f3ae5a0140d
edberaga <edbetjonnathan@gmail.com>
2024-10-18 01:09:34 +0700

    Code the table to retireve API from replit

--- src/pages/Tables.jsx
+++ src/pages/Tables.jsx
@@ -1,7 +1,9 @@
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import axios from 'axios';
 import { Container, ListGroup } from 'react-bootstrap';
 import booking from '../data/Tables.json'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import { useEffect, useState } from 'react';
 
 const Card = ({ no, booked, customer_name, book_time}) => {
   return(
@@ -33,11 +35,29 @@ const Card = ({ no, booked, customer_name, book_time}) => {
 }
 
 const Tables = () => {
+  const [table, setTable] = useState([]);
+  const url = 'https://ad05cf14-c6a1-47ee-bf07-5e67270d988b-00-1l68yogb2dq5j.sisko.replit.dev/meja';
+
+  // Fetch events
+  const fetchEvents = async () => {
+    try {
+      const response = await axios.get(url);
+      setTable(response.data);
+      console.log('Fetched events:', response.data);
+    } catch (error) {
+      console.error('Error fetching events:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchEvents();
+  }, []);
+
   return (
   <section className='container py-3 text-center d-flex justify-content-center'>
     <Container className="row row-cols-1 row-cols-xl-3 mb-3 ">
-      {booking.map((item) => {
-        return <Card key={item.no} no={item.no} booked={item.booked} customer_name={item.customer_name} book_time={item.book_time}/>;
+      {table.map((item) => {
+        return <Card key={item.id} no={item.id} booked={item.booked} customer_name={item.customer_name} book_time={item.book_time}/>;
       })}
     </Container>
   </section>
