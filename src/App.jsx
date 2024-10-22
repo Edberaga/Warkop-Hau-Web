899581f6792bc27da12a027d6758a746225c09e9
edberaga <edbetjonnathan@gmail.com>
2024-10-23 01:14:32 +0700

    Add the react router and the toastify

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-
 import './App.css'
-import Tables from './pages/Tables'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import Tables from './pages/Tables';
+import Login from './pages/Login';
 
 function App() {
-  
-
   return (
   <main>
-    <Tables/>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Tables />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+    <ToastContainer />
   </main>
   )
 }
