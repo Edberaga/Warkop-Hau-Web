74c58c884c079c3dfc2aba27241eff4dc6281766
edberaga <edbetjonnathan@gmail.com>
2024-10-24 23:15:36 +0700

    add the Register path

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import Tables from './pages/Tables';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Tables />} />
         <Route path="/login" element={<Login />} />
+        <Route path='/register' element={<Register />} />
       </Routes>
     </Router>
     <ToastContainer />
