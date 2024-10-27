829014d42915482a59146d107769fcacb07395b5
edberaga <edbetjonnathan@gmail.com>
2024-10-27 13:27:46 +0700

    add homepage routine

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import Home from './pages/Homepage';
 import Tables from './pages/Tables';
 import Login from './pages/Login';
 import Register from './pages/Register';
 
 function App() {
   return (
-  <main>
+  <main className='h-full bg-pattern bg-repeat max-w-[1800px] mx-auto overflow-hidden'>
     <Router>
       <Routes>
-        <Route path="/" element={<Tables />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/buku-meja" element={<Tables />} />
         <Route path="/login" element={<Login />} />
         <Route path='/register' element={<Register />} />
       </Routes>
