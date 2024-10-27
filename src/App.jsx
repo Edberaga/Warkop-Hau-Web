c36dbba78f3de74d2685bd91746800ddc4feecf9
edberaga <edbetjonnathan@gmail.com>
2024-10-28 01:29:26 +0700

    Add header to the web

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,16 @@ import Home from './pages/Homepage';
 import Tables from './pages/Tables';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Header from './components/Header';
 
 function App() {
   return (
   <main className='h-full bg-pattern bg-repeat max-w-[1800px] mx-auto overflow-hidden'>
     <Router>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
+
         <Route path="/buku-meja" element={<Tables />} />
         <Route path="/login" element={<Login />} />
         <Route path='/register' element={<Register />} />
